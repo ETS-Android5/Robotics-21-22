2a8987fb18250defb0fe1581042152281f022381
Yeongjin Kwon <yeongjinkwon@gmail.com>
2021-12-01 01:07:41 -0500

    First TeleOp mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleOp.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// This class contains the code for the main competition TeleOp.
+
+@TeleOp(name="Main TeleOp", group="Main")
+public class CompetitionTeleOp extends LinearOpMode {
+
+    // Declare members
+    private final FourWheelRobot robot;
+
+    public ExampleTeleOp() {
+        super();
+        robot = new FourWheelRobot(hardwareMap);
+    }
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Controller loop
+            if (GamepadUtil.leftTriggerPressed(gamepad1)) {
+                robot.rotate(-0.5);
+            }
+            else if (GamepadUtil.rightTriggerPressed(gamepad1)) {
+                robot.rotate(0.5);
+            }
+            else {
+                robot.translate(gamepad1.right_stick_x, -1*gamepad1.left_stick_y);
+            }
+        }
+    }
+}
