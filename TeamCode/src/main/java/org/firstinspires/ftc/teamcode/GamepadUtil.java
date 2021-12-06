72c8541e462009918019528bc1392daeed37a79c
Robotics computer <ykwon23@madisonnjps.org>
2021-12-06 14:58:05 -0500

    Fix bug with gamepad trigger helper code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadUtil.java
@@ -4,9 +4,9 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class GamepadUtil {
     public static boolean leftTriggerPressed(Gamepad gamepad) {
-        return gamepad.left_trigger < 0.1;
+        return gamepad.left_trigger > 0.85;
     }
     public static boolean rightTriggerPressed(Gamepad gamepad) {
-        return gamepad.right_trigger < 0.1;
+        return gamepad.right_trigger > 0.85;
     }
 }
