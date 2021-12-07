7e64b54e25dc0730ea6fe7c4546e94312b60585c
Yeongjin Kwon <yeongjinkwon@gmail.com>
2021-12-07 18:53:37 +0000

    WIP make servo stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotUtil.java
@@ -3,10 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class RobotUtil {
 
-    // This function takes a hardware map instance,
+    // This function gets, resets, and initializes a motor.
+    // It takes a hardware map instance,
     // some motor parameters, and returns a DcMotor.
     public static DcMotor getDcMotor(
         HardwareMap hardwareMap,
@@ -16,10 +18,23 @@ public class RobotUtil {
         DcMotor.RunMode runMode
     ) {
         DcMotor motor = hardwareMap.get(DcMotor.class, motorName);
+        motor.resetDeviceConfigurationForOpMode();
         motor.setDirection(direction);
         motor.setZeroPowerBehavior(ZPB);
         motor.setMode(runMode);
         return motor;
+    }
+
+    // This function gets, resets, and initializes a servo.
+    public static Servo getServo(
+        HardwareMap hardwareMap,
+        String servoName,
+        Servo.Direction direction
+    ) {
+        Servo servo = hardwareMap.get(Servo.class, servoName);
+        servo.resetDeviceConfigurationForOpMode();
+        servo.setDirection(direction);
+        return servo;
     }
 
 }
