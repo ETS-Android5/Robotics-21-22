ecb53e7e3c37b11a9348c38536029147e1c9cf84
Yeongjin Kwon <yeongjinkwon@gmail.com>
2021-12-15 16:34:39 +0000

    Optimize Vector2d well

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vector2d.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vector2d.kt
@@ -1,51 +1,21 @@
 package org.firstinspires.ftc.teamcode
 
-import com.qualcomm.robotcore.hardware.HardwareMap
-import com.qualcomm.robotcore.hardware.DcMotorSimple
-import com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior
-import com.qualcomm.robotcore.hardware.DcMotor.RunMode
-import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.Servo
-import com.qualcomm.robotcore.hardware.CRServo
-import org.firstinspires.ftc.teamcode.Vector2d
+data class Vector2d(val x: Double = 0.0, val y: Double = 0.0) {
 
-// This class used to use vecmath.Vector2d by extending it,
-// but since the vecmath package was having trouble integrating into
-// the robotics code build environment, vecmath was just removed instead.
-class Vector2d /* extends javax.vecmath.Vector2d */ {
-    var x = 0.0
-    var y = 0.0
-    operator fun set(x: Double, y: Double) {
-        this.x = x
-        this.y = y
-    }
-
-    constructor() {}
-    constructor(x: Double, y: Double) {
-        set(x, y)
-    }
-
-    // Rotate vector in place
-    // Positive angle is counterclockwise
-    // Angle measured in radians
-    fun rotate(angle: Double): Vector2d {
-        x = x * Math.cos(angle) - y * Math.sin(angle)
-        y = y * Math.cos(angle) + x * Math.sin(angle)
-        return this
-    }
+    // Return a new vector that is rotated by an angle.
+    // Positive angle is counterclockwise.
+    // Angle measured in radians.
+    infix fun rotate(angle: Double) = Vector2d(
+        x = x * Math.cos(angle) - y * Math.sin(angle),
+        y = y * Math.cos(angle) + x * Math.sin(angle),
+    )
 
     companion object {
         // Static helper functions
-        fun construct(): Vector2d {
-            return Vector2d()
-        }
-
-        fun construct(x: Double, y: Double): Vector2d {
-            return Vector2d(x, y)
-        }
-
-        fun polar(r: Double, theta: Double): Vector2d {
-            return construct(r, 0.0).rotate(theta)
-        }
+        // This will probably generate a syntax error, cause I think "this" here
+        // refers to the companion object, not the Vector2d constructor.
+        // This function allows you to get a vector using polar coordinates
+        // instead of cartesian coordinates.
+        fun polar(r: Double, theta: Double) = this(r, 0.0).rotate(theta)
     }
 }
