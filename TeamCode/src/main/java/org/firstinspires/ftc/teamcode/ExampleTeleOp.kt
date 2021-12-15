3042cf50097953e0f4bc8161016e796bcc27a72d
Robotics computer <ykwon23@madisonnjps.org>
2021-12-15 14:54:21 -0500

    More WIP stuff (look over this, removed some TODO tags)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleTeleOp.kt
@@ -6,8 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 @TeleOp(name = "Example TeleOp", group = "Example Group")
 // Test, this will probably cause a syntax error cause
 // you need to explicitly construct the baseclass one place or another
-class ExampleTeleOp : LinearOpMode {
-    init TODO("Testing if above works")
+class ExampleTeleOp : LinearOpMode() {
     // Declare members
     private lateinit var robot: FourWheelRobot
 
@@ -22,10 +21,9 @@ class ExampleTeleOp : LinearOpMode {
         waitForStart()
         while (opModeIsActive()) {
             // Controller loop
-            TODO("Another test for if the . operator without stuff on left works")
-            robot.rotate(when (gamepad1) {
-                .leftTriggerPressed() -> -0.5
-                .rightTriggerPressed() -> 0.5
+            robot.rotate(when {
+                gamepad1.leftTriggerPressed() -> -0.5
+                gamepad1.rightTriggerPressed() -> 0.5
                 else -> 0.0
             })
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadUtil.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadUtil.kt
@@ -2,6 +2,6 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.hardware.Gamepad
 
-fun Gamepad.leftTriggerPressed() = gamepad.left_trigger > 0.85
+fun Gamepad.leftTriggerPressed() = left_trigger > 0.85
 
-fun Gamepad.rightTriggerPressed() = gamepad.right_trigger > 0.85
+fun Gamepad.rightTriggerPressed() = right_trigger > 0.85
