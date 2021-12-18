bc6c7ebd2f1308a2f1c02e90c47a3ed461228aa6
Robotics computer <ykwon23@madisonnjps.org>
2021-12-18 14:38:19 -0500

    Solve tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util.kt
@@ -1,21 +1,20 @@
 package org.firstinspires.ftc.teamcode
 
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
 class NonNullWriteOnce<T : Any> : ReadWriteProperty<Any?, T> {
+    
+    private var property: T? = null
 
-    TEST()
-    private property: T?
-
-    override fun getProperty(thisRef: Any?, kProp: KProperty<*>): T =
-        property :? throw IllegalStateException(
+    override fun getValue(thisRef: Any?, kProp: KProperty<*>): T =
+        property ?: throw IllegalStateException(
             "The property \"${kProp.name}\" from \"$thisRef\" "+
             "was accessed before it was initialized."
         )
-    TEST()
-    override fun setProperty(_: Any?, _: KProperty<*>, value: T) {
-        TEST()
-        if (property == null) property = value else true // throw IllegalStateException(
+    override fun setValue(thisRef: Any?, kProp: KProperty<*>, value: T) =
+        if (property == null) property = value else throw IllegalStateException(
             "The property \"${kProp.name}\" from \"$thisRef\" "+
             "was written to after already being initialized once."
         )
-    }
 }
