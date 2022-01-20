4020343076c3e274ed5bce80f4f8f056869e2424
Robotics computer <ykwon23@madisonnjps.org>
2022-01-20 16:50:47 -0500

    Improve example code style and add GadgetsRobot

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/gadgets/GadgetsRobot.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/gadgets/GadgetsRobot.kt
@@ -1,13 +1,17 @@
-package org.firstinspires.ftc.teamcode.example
+package org.firstinspires.ftc.teamcode.gadgets
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple.Direction as DcDirection
 import com.qualcomm.robotcore.hardware.HardwareMap
 
 import org.firstinspires.ftc.teamcode.common.FourWheelRobot
 
-class ExampleRobot(hardwareMap: HardwareMap) : FourWheelRobot(hardwareMap) {
+class GadgetsRobot(hardwareMap: HardwareMap) : FourWheelRobot(hardwareMap) {
     override val leftFront = getWheel("leftFront", DcDirection.FORWARD)
     override val rightFront = getWheel("rightFront", DcDirection.FORWARD)
-    override val leftRear = getWheel("leftRear", DcDirection.FORWARD)
+    override val leftRear = getWheel("leftRear", DcDireckjktion.FORWARD)
     override val rightRear = getWheel("rightRear", DcDirection.FORWARD)
+
+    override fun translate(px: Double, py: Double): FourWheelRobot {
+        return this
+    }
 }
