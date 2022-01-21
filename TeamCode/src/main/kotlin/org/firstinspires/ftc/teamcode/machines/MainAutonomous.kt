7d2d2ccd17770e4097dd976dcf826d9a81c20d3b
Yeongjin Kwon <yeongjinkwon@gmail.com>
2022-01-21 18:34:11 +0000

    Create main autonomous for mad machines

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/machines/MainAutonomous.kt
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.machines
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+import org.firstinspires.ftc.teamcode.common.LateInitConstProperty
+
+@Autonomous(name = "Main Autonomous", group = "Autonomous")
+class MainAutonomous : LinearOpMode() {
+    // Declare members
+    private var robot: MadMachinesRobot by LateInitConstProperty()
+
+    override fun runOpMode() = runBlocking {
+        robot = MadMachinesRobot(hardwareMap)
+
+        telemetry.addData("Status", "Initialized")
+        telemetry.update()
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart()
+
+        robot.translate(0.0, 0.25)
+        delay(1500)
+        robot.translate(0.0, -0.25)
+        delay(1500)
+        robot.translate(0.0, 0.0)
+    }
+}
