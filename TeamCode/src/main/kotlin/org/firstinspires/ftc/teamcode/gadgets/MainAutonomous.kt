67e9f1b8ac53191c900270b9e2c007cb95f4e56a
Avinash Goyal <99211613+Rogue852@users.noreply.github.com>
2022-02-14 19:48:26 +0000

    Created basic movement for the gadgets autonomous mode. The delay times are estimates and not actual values.

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/gadgets/MainAutonomous.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/gadgets/MainAutonomous.kt
@@ -19,13 +19,49 @@ class MainAutonomous : LinearOpMode() {
         telemetry.addData("Status", "Initialized")
         telemetry.update()
 
+        launch {
+          delay(5000)
+          telemetry.addData("Test", )h
+        }
+
         // Wait for the game to start (driver presses PLAY)
+
         waitForStart()
 
+        //Younjin auntanamous
+
+        //robot.translate(0.0, 0.25)
+        //delay(1500)
+        //robot.translate(0.0, -0.25)
+        //delay(1500)
+        //robot.translate(0.0, 0.0)
+
+        //Avi auntanamous
+
+        //Move to carasol
         robot.translate(0.0, 0.25)
         delay(1500)
+        robot.translate(0.0, 0.0)
+
+        //Insert place on shipping hub
+
+        robot.rotate(-0.25)
+        delay(1500)
+        robot.rotate(0.0)
+
+        //Reverse to carasol
         robot.translate(0.0, -0.25)
         delay(1500)
         robot.translate(0.0, 0.0)
+
+        //Spin carasol
+
+        //Move to shipping hub
+        robot.translate(0.0, 0.25)
+        delay(4500)
+
+
+
+        
     }
 }
